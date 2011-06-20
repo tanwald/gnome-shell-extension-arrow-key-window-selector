399ad48be2bc477ae880e66145b502ff6527e6bd
Paul Neulinger <paul.neulinger@tanwald.net>
2011-06-20 14:51:59 +0200

    zoom selected works

--- extension.js
+++ extension.js
@@ -6,7 +6,6 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Workspace = imports.ui.workspace;
 const WorkspacesView = imports.ui.workspacesView;
-const Tweener = imports.ui.tweener;
 
 function injectToFunction(parent, name, func) {
     let origin = parent[name];
@@ -21,15 +20,20 @@ function injectToFunction(parent, name, func) {
 
 function main() {
 
-    Workspace.Workspace.prototype.getMetaWindow = function(index) {
+    Workspace.Workspace.prototype.getVisibleClone = function(index) {
     	let visibleClones = this._windows.slice();
         if (this._reservedSlot) {
         	visibleClones.push(this._reservedSlot);
         }
-
         let slots = this._computeAllWindowSlots(visibleClones.length);
         visibleClones = this._orderWindowsByMotionAndStartup(visibleClones, slots);
-        return visibleClones[index].metaWindow;
+        return visibleClones[index];
+    }
+    
+    Workspace.WindowClone.prototype.zoomSelected = function() {
+        this._zoomStart();
+        this._zoomStep += 25;
+        this._zoomUpdate();
     }
 
     WorkspacesView.WorkspacesView.prototype._onArrowKeyPress = function(s, o) {
@@ -38,25 +42,30 @@ function main() {
         let workspace = this._workspaces[active];
         if (key != Clutter.Up && key != Clutter.Down && key != Clutter.Left && key != Clutter.Right) {
             if (key == Clutter.Return) {
-                let win = workspace.getMetaWindow(this._arrowKeyIndex);
+                let win = workspace.getVisibleClone(this._arrowKeyIndex);
                 if (win) {
-                    Main.activateWindow(win, global.get_current_time());
+                    Main.activateWindow(win.metaWindow, global.get_current_time());
                  }
             }
             return false;
         }
         let cols = workspace.geometryDict.cols;
-        global.log(cols);
-        if (key == Clutter.Up) {
-        	this._arrowKeyIndex -= cols;
-        } else if (key == Clutter.Down) {
-        	this._arrowKeyIndex += cols;
-        } else if (key == Clutter.Left) {
-            --this._arrowKeyIndex;
-        } else if (key == Clutter.Right) {
-            ++this._arrowKeyIndex;
-        }
-        global.log(this._arrowKeyIndex);
+        //global.log(cols);
+        if (this._prevSelectedWin) {
+        	this._prevSelectedWin._zoomEnd();
+            if (key == Clutter.Up) {
+            	this._arrowKeyIndex -= cols;
+            } else if (key == Clutter.Down) {
+            	this._arrowKeyIndex += cols;
+            } else if (key == Clutter.Left) {
+                --this._arrowKeyIndex;
+            } else if (key == Clutter.Right) {
+                ++this._arrowKeyIndex;
+            }
+        } 
+        this._prevSelectedWin = workspace.getVisibleClone(this._arrowKeyIndex);
+        this._prevSelectedWin.zoomSelected();
+        //global.log(this._arrowKeyIndex);
         return true;
     }
 
@@ -69,16 +78,15 @@ function main() {
         if (this._reservedSlot) {
         	visibleClones.push(this._reservedSlot);
         }
-
         let slots = this._computeAllWindowSlots(visibleClones.length);
         visibleClones = this._orderWindowsByMotionAndStartup(visibleClones, slots);
         let y = slots[0][1];
         let cols = 0;
         let firstRow = true;
-        
+        //global.log(visibleClones.length);
         for (let i = 0; i < visibleClones.length; i++) {
-//        	global.log(slots[i][0]);
-//        	global.log(slots[i][1]);
+        	//global.log(slots[i][0]);
+        	//global.log(slots[i][1]);
             if (y == slots[i][1] && firstRow) {
                 ++cols;
             } else {
@@ -86,6 +94,7 @@ function main() {
             }
             let clone = visibleClones[i];
             let metaWindow = clone.metaWindow;
+            //global.log(metaWindow.title);
             let mainIndex = this._lookupIndex(metaWindow);
         }
         this.geometryDict.cols = cols;
@@ -93,6 +102,7 @@ function main() {
 
     injectToFunction(WorkspacesView.WorkspacesView.prototype, '_init', function(width, height, x, y, workspaces) {
         this._arrowKeyIndex = 0;
+        this._prevSelectedWin = null;
         this._arrowKeyPressEventId = global.stage.connect('key-press-event', Lang.bind(this, this._onArrowKeyPress));
     });
     
@@ -100,6 +110,5 @@ function main() {
         global.stage.disconnect(this._arrowKeyPressEventId);
     });
 }
-
 
 
